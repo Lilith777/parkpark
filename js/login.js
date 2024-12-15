54977cd73ce665bc0edfbd29a1a2d91b529724fb
Lilith777 <45128234+Lilith777@users.noreply.github.com>
2024-12-16 05:51:47 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // 배경색 변경 함수
     function changeBackgroundColor() {
-        const colors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#FFFF33"];
+        const colors = [ "#ef5777","#575fcf","#4bcffa","#34e7e4","#0be881","#f53b57","#3c40c6","#0fbcf9","#00d8d6","#05c46b","#ffc048","#ffdd59","#ff5e57","#d2dae2","#485460","#ffa801","#ffd32a","#ff3f34"];
         const randomColor = colors[Math.floor(Math.random() * colors.length)];
         console.log("배경색 변경: " + randomColor); // 배경색 변경 로그 출력
         
